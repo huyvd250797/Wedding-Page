ed8a8207f681c1b83f1f3fb7c41c2f4a67a2be74
Huy Vo <huyvd250797@gmail.com>
2024-09-03 23:54:58 +0700

    Update btn Join & btn Send Wish

--- javascript.js
+++ javascript.js
@@ -5,3 +5,15 @@ const tooltipTriggerList = document.querySelectorAll(
 const tooltipList = [...tooltipTriggerList].map(
   (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
+
+// Show btn Send
+const registryWishInputFocus = () => {
+  let getBtnSend = document.getElementById("registryBtnSend");
+  getBtnSend.classList.remove("opacity-0");
+};
+
+// Hide btn Send
+const registryWishInputBlur = () => {
+  let getBtnSend = document.getElementById("registryBtnSend");
+  getBtnSend.classList.add("opacity-0");
+};
