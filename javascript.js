f14a388a70c8f152fed133f25280f3569b30c787
Huy Vo <huyvd250797@gmail.com>
2024-09-07 20:59:34 +0700

    Added cover photo

--- javascript.js
+++ javascript.js
@@ -17,3 +17,14 @@ const registryWishInputBlur = () => {
   let getBtnSend = document.getElementById("registryBtnSend");
   getBtnSend.classList.add("opacity-0");
 };
+
+//!  --------------------------- Parallax scroll -------------------------- */
+
+let paraScrollItems = document.querySelectorAll(".item-paraScroll");
+document.addEventListener("scroll", (e) => {
+  paraScrollItems.forEach((item) => {
+    if (item.offsetTop - window.scrollY < 350) {
+      item.classList.add("active");
+    }
+  });
+});
