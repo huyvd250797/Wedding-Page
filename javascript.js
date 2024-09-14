00a039d476d165557974804ce0cbf2de2df9cf75
Huy Vo <huyvd250797@gmail.com>
2024-09-15 00:30:16 +0700

    Added effect to INTRODUCE & PHOTO

--- javascript.js
+++ javascript.js
@@ -23,8 +23,59 @@ const registryWishInputBlur = () => {
 let paraScrollItems = document.querySelectorAll(".item-paraScroll");
 document.addEventListener("scroll", (e) => {
   paraScrollItems.forEach((item) => {
-    if (item.offsetTop - window.scrollY < 350) {
+    if (item.offsetTop - window.scrollY < 400) {
       item.classList.add("active");
     }
   });
 });
+
+// INTRODUCE
+let paraScrollItemsIntro = document.querySelectorAll(
+  ".item-paraScroll-introduce"
+);
+document.addEventListener("scroll", (e) => {
+  paraScrollItemsIntro.forEach((item) => {
+    if (item.offsetTop - window.scrollY < 560) {
+      item.classList.add("active");
+    }
+  });
+});
+
+// PHOTO
+let paraScrollItemsPhoto = document.querySelectorAll(".item-paraScroll-photo");
+document.addEventListener("scroll", (e) => {
+  paraScrollItemsPhoto.forEach((item) => {
+    if (item.offsetTop - window.scrollY < 800) {
+      item.classList.add("active");
+      randDisplay();
+    }
+  });
+});
+
+// Hàm random thứ tự hiển thị
+const randDisplay = () => {
+  let getItemPhoto = document.querySelectorAll(".photo-album-img");
+
+  let getItem = [];
+
+  for (let i = 1; i < 9; i++) {
+    getItem.push(i * Math.floor(1 + Math.random()));
+  }
+
+  //?!! sort random index element off array getItem
+  // getItem.sort(() => Math.random() - 0.5
+  let randomIndexArr = getItem.sort(() => Math.random() - 0.5);
+
+  for (let i = 0; i < getItemPhoto.length; i++) {
+    if (getItemPhoto[i].classList.contains("active")) {
+      break;
+    } else {
+      // add class active
+      getItemPhoto[i].classList.add(`active`);
+
+      // add class delay với giá trị là giá trị index mảng random
+      getItemPhoto[i].classList.add(`delay-${randomIndexArr[i]}`);
+    }
+    console.log(getItemPhoto[i]);
+  }
+};
