0735eb5f81f7f2448982431571bd0c725e919646
Huy Vo <huyvd250797@gmail.com>
2024-09-21 16:52:59 +0700

    Responsive PHOTO

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
+//? --------------------- Khai báo độ phân giải màn hình --------------------- */
+let largePc = window.matchMedia("(min-width: 1900px)");
+let pc = window.matchMedia("(min-width: 1024px)");
+let tabletPC = window.matchMedia("(min-width: 740px)");
+let tablet = window.matchMedia("(min-width: 740px) and (max-width: 1023px)");
+let mobile = window.matchMedia("(max-width: 1023px)");
+
 //* tool tip
 const tooltipTriggerList = document.querySelectorAll(
   '[data-bs-toggle="tooltip"]'
@@ -20,37 +27,206 @@ const registryWishInputBlur = () => {
 
 //!  --------------------------- Parallax scroll -------------------------- */
 
-let paraScrollItems = document.querySelectorAll(".item-paraScroll");
-document.addEventListener("scroll", (e) => {
-  paraScrollItems.forEach((item) => {
-    if (item.offsetTop - window.scrollY < 400) {
-      item.classList.add("active");
-    }
+//? Nếu độ phân giải màn hình là PC, thì thực hiện animation PC
+if (pc.matches) {
+  // common
+  let paraScrollItems = document.querySelectorAll(".item-paraScroll");
+  document.addEventListener("scroll", (e) => {
+    paraScrollItems.forEach((item) => {
+      if (item.offsetTop - window.scrollY < 400) {
+        item.classList.add("active");
+      }
+    });
   });
-});
 
-// INTRODUCE
-let paraScrollItemsIntro = document.querySelectorAll(
-  ".item-paraScroll-introduce"
-);
-document.addEventListener("scroll", (e) => {
-  paraScrollItemsIntro.forEach((item) => {
-    if (item.offsetTop - window.scrollY < 560) {
-      item.classList.add("active");
-    }
+  // INTRODUCE
+  let paraScrollItemsIntro = document.querySelectorAll(
+    ".item-paraScroll-introduce"
+  );
+  document.addEventListener("scroll", (e) => {
+    paraScrollItemsIntro.forEach((item) => {
+      if (item.offsetTop - window.scrollY < 560) {
+        item.classList.add("active");
+      }
+    });
   });
-});
 
-// PHOTO
-let paraScrollItemsPhoto = document.querySelectorAll(".item-paraScroll-photo");
-document.addEventListener("scroll", (e) => {
-  paraScrollItemsPhoto.forEach((item) => {
-    if (item.offsetTop - window.scrollY < 800) {
-      item.classList.add("active");
-      randDisplay();
-    }
+  // PHOTO
+  let paraScrollItemsPhoto = document.querySelectorAll(
+    ".item-paraScroll-photo"
+  );
+  document.addEventListener("scroll", (e) => {
+    paraScrollItemsPhoto.forEach((item) => {
+      if (item.offsetTop - window.scrollY < 800) {
+        item.classList.add("active");
+        randDisplay();
+      }
+    });
   });
-});
+}
+
+//TODO --------------------------------- MOBILE --------------------------------- */
+
+//? Nếu độ phân giải màn hình là Mobile, thì thực hiện animation Mobile
+else if (mobile.matches) {
+  // option1
+  let paraScrollItems = document.querySelectorAll(".item-paraScroll");
+  document.addEventListener("scroll", (e) => {
+    paraScrollItems.forEach((item) => {
+      if (item.offsetTop - window.scrollY < 600) {
+        item.classList.add("active");
+      }
+    });
+  });
+
+  // option2
+  let paraScrollItems2 = document.querySelectorAll(".item-paraScroll2");
+  document.addEventListener("scroll", (e) => {
+    paraScrollItems2.forEach((item) => {
+      if (item.offsetTop - window.scrollY < 600) {
+        item.classList.add("active");
+      }
+    });
+  });
+
+  // option3
+  let paraScrollItems3 = document.querySelectorAll(".item-paraScroll3");
+  document.addEventListener("scroll", (e) => {
+    paraScrollItems3.forEach((item) => {
+      if (item.offsetTop - window.scrollY < 400) {
+        item.classList.add("active");
+      }
+    });
+  });
+
+  // option4
+  let paraScrollItems4 = document.querySelectorAll(".item-paraScroll4");
+  document.addEventListener("scroll", (e) => {
+    paraScrollItems4.forEach((item) => {
+      if (item.offsetTop - window.scrollY < 300) {
+        item.classList.add("active");
+      }
+    });
+  });
+
+  // option5
+  let paraScrollItems5 = document.querySelectorAll(".item-paraScroll5");
+  document.addEventListener("scroll", (e) => {
+    paraScrollItems5.forEach((item) => {
+      if (item.offsetTop - window.scrollY < 400) {
+        item.classList.add("active");
+      }
+    });
+  });
+
+  // INTRODUCE
+  let paraScrollItemsIntro = document.querySelectorAll(
+    ".item-paraScroll-introduce"
+  );
+  document.addEventListener("scroll", (e) => {
+    paraScrollItemsIntro.forEach((item) => {
+      if (item.offsetTop - window.scrollY < 700) {
+        item.classList.add("active");
+      }
+    });
+  });
+
+  // PHOTO
+  let paraScrollItemsPhoto = document.querySelectorAll(
+    ".item-paraScroll-photo"
+  );
+  document.addEventListener("scroll", (e) => {
+    paraScrollItemsPhoto.forEach((item) => {
+      if (item.offsetTop - window.scrollY < 500) {
+        item.classList.add("active");
+      }
+    });
+  });
+
+  // PHOTO2
+  let paraScrollItemsPhoto2 = document.querySelectorAll(
+    ".item-paraScroll-photo2"
+  );
+  document.addEventListener("scroll", (e) => {
+    paraScrollItemsPhoto2.forEach((item) => {
+      if (item.offsetTop - window.scrollY < 400) {
+        item.classList.add("active");
+      }
+    });
+  });
+
+  // PHOTO3
+  let paraScrollItemsPhoto3 = document.querySelectorAll(
+    ".item-paraScroll-photo3"
+  );
+  document.addEventListener("scroll", (e) => {
+    paraScrollItemsPhoto3.forEach((item) => {
+      if (item.offsetTop - window.scrollY < 400) {
+        item.classList.add("active");
+      }
+    });
+  });
+
+  // PHOTO4
+  let paraScrollItemsPhoto4 = document.querySelectorAll(
+    ".item-paraScroll-photo4"
+  );
+  document.addEventListener("scroll", (e) => {
+    paraScrollItemsPhoto4.forEach((item) => {
+      if (item.offsetTop - window.scrollY < 400) {
+        item.classList.add("active");
+      }
+    });
+  });
+
+  // PHOTO5
+  let paraScrollItemsPhoto5 = document.querySelectorAll(
+    ".item-paraScroll-photo5"
+  );
+  document.addEventListener("scroll", (e) => {
+    paraScrollItemsPhoto5.forEach((item) => {
+      if (item.offsetTop - window.scrollY < 400) {
+        item.classList.add("active");
+      }
+    });
+  });
+
+  // PHOTO6
+  let paraScrollItemsPhoto6 = document.querySelectorAll(
+    ".item-paraScroll-photo6"
+  );
+  document.addEventListener("scroll", (e) => {
+    paraScrollItemsPhoto6.forEach((item) => {
+      if (item.offsetTop - window.scrollY < 400) {
+        item.classList.add("active");
+      }
+    });
+  });
+
+  // PHOTO7
+  let paraScrollItemsPhoto7 = document.querySelectorAll(
+    ".item-paraScroll-photo7"
+  );
+  document.addEventListener("scroll", (e) => {
+    paraScrollItemsPhoto7.forEach((item) => {
+      if (item.offsetTop - window.scrollY < 400) {
+        item.classList.add("active");
+      }
+    });
+  });
+
+  // PHOTO8
+  let paraScrollItemsPhoto8 = document.querySelectorAll(
+    ".item-paraScroll-photo8"
+  );
+  document.addEventListener("scroll", (e) => {
+    paraScrollItemsPhoto8.forEach((item) => {
+      if (item.offsetTop - window.scrollY < 400) {
+        item.classList.add("active");
+      }
+    });
+  });
+}
 
 // Hàm random thứ tự hiển thị
 const randDisplay = () => {
